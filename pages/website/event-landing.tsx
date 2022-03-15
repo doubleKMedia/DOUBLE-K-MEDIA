fe5789a83dbc1c24c5295089a3e05a9bd2d268d7
origin <chldlsrb1000@naver.com>
2022-03-15 22:31:43 +0900

    event-landing 페이지 생성

---
+++ pages/website/event-landing.tsx
@@ -0,0 +1,42 @@
+import { NextPage } from 'next';
+import ScrollListView from '../../components/website/ScrollListView';
+import WebsiteLayout from '../../components/website/WebsiteLayout';
+import scrollListData from './event-landing/scrollListData.json';
+
+const EventLanding: NextPage = () => {
+  return (
+    <WebsiteLayout>
+      <div className="homepage">
+        <article className="customized">
+          <ScrollListView listData={scrollListData} link="/website/project-inquiry" />
+        </article>
+      </div>
+
+      <style jsx>
+        {`
+          .homepage {
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+          }
+
+          .customized {
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            box-sizing: border-box;
+            background-color: white;
+          }
+        `}
+      </style>
+    </WebsiteLayout>
+  );
+};
+
+export default EventLanding;
