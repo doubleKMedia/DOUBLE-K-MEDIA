b1f0fa9986189768df088aa9ca5321c5cb56dcec
origin <chldlsrb1000@naver.com>
2022-03-15 13:15:11 +0900

    website 페이지 설정 - redirect 추가 및 기존 index 페이지 삭제

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  redirects: async () => {
+    return [
+      {
+        source: '/website',
+        destination: '/website/homepage',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
