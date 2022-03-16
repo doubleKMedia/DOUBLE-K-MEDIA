a176372778b3668f20ccdcd3df711b3241ec4e82
origin <chldlsrb1000@naver.com>
2022-03-16 19:21:30 +0900

    디자인 및 샘플 페이지 생성 - 각 데이터 분리하여 추가

---
+++ pages/website/design.tsx
@@ -0,0 +1,57 @@
+import { NextPage } from 'next';
+import LineListView from '../../components/website/LineListView';
+import WebsiteLayout from '../../components/website/WebsiteLayout';
+import lineListData from './design/lineListData.json';
+
+const Design: NextPage = () => {
+  return (
+    <WebsiteLayout>
+      <div className="design">
+        <div className="head">
+          <span>기타 디자인 샘플</span>
+          <span>
+            홈페이지 배너, SNS 배너, 상세페이지, 브랜딩 작업 등의 디자인 작업입니다.
+            <br />
+            해당 작업은 별도 문의 주시기 바랍니다.
+          </span>
+        </div>
+        <LineListView listData={lineListData} />
+      </div>
+
+      <style jsx>
+        {`
+          .design {
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            padding: 100px 0;
+          }
+
+          .head {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            gap: 10px;
+            text-align: center;
+          }
+
+          .head > span:nth-child(1) {
+            font-size: 30px;
+            font-weight: bold;
+          }
+
+          .head > span:nth-child(2) {
+            font-size: 18px;
+            color: #6a6a6a;
+          }
+        `}
+      </style>
+    </WebsiteLayout>
+  );
+};
+
+export default Design;
---
+++ pages/website/sample.tsx
@@ -0,0 +1,53 @@
+import { NextPage } from 'next';
+import LineListView from '../../components/website/LineListView';
+import WebsiteLayout from '../../components/website/WebsiteLayout';
+import lineListData from './sample/lineListData.json';
+
+const Sample: NextPage = () => {
+  return (
+    <WebsiteLayout>
+      <div className="sample">
+        <div className="head">
+          <span>샘플 보기</span>
+          <span>더블케이미디어 작업물 샘플</span>
+        </div>
+        <LineListView listData={lineListData} />
+      </div>
+
+      <style jsx>
+        {`
+          .sample {
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            padding: 100px 0;
+          }
+
+          .head {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            gap: 10px;
+            text-align: center;
+          }
+
+          .head > span:nth-child(1) {
+            font-size: 30px;
+            font-weight: bold;
+          }
+
+          .head > span:nth-child(2) {
+            font-size: 18px;
+            color: #6a6a6a;
+          }
+        `}
+      </style>
+    </WebsiteLayout>
+  );
+};
+
+export default Sample;
