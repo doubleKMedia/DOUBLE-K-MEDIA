0af1ac3974c8a89d99fc65f8b3bb49ca72cbf90f
origin <chldlsrb1000@naver.com>
2022-03-18 00:12:50 +0900

    테스트용 코드 추가 - formidable 라이브러리 추가 및 api hello에서 데이터 확인 코드 추가

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { inputValueType } from '../website/project-inquiry';
+import formidable, { IncomingForm } from 'formidable';
+import { writeFileSync } from 'fs';
+import PersistentFile from 'formidable/PersistentFile';
 
-type Data = {
-  name: string
-}
+export const config = {
+  api: {
+    bodyParser: false,
+    // bodyParser: {
+    //   sizeLimit: '40mb',
+    // },
+  },
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const form = new IncomingForm();
+
+  form.parse(req, (err, fields, files) => {
+    if (err) res.status(500).json({ result: err });
+
+    if (typeof fields.data === 'string') console.log(JSON.parse(fields.data));
+    console.log(files.file);
+  });
+
+  res.status(200).json({ result: 'success!' });
 }
